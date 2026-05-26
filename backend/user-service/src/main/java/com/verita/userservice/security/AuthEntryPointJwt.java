8ab063454db39b71983af65327016655292c15c1
Arpad-H <horvaarpad@gmail.com>
2026-05-26 20:15:02 +0200

    Added authentication
    Added AuthenticationController for http endpoint, AuthService for auth buisness logic adn jwt itnerfacing

---
+++ backend/user-service/src/main/java/com/verita/userservice/security/AuthEntryPointJwt.java
@@ -0,0 +1,27 @@
+package com.verita.userservice.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+//handles unauthenticated access. It returns HTTP 401 instead of redirecting to a login page
+@Component
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        logger.error("Unauthorized error: {}", authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+    }
+}
+
