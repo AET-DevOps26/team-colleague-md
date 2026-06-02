7449fa841c9113892917b61b7d3366610f2b9279
Arpad-H <horvaarpad@gmail.com>
2026-06-02 18:54:31 +0200

    added enteties for comments, bookmarks aand timestamps

---
+++ backend/content-service/src/main/java/com/verita/contentservice/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.verita.contentservice;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.OffsetDateTime;
+@MappedSuperclass
+abstract class BaseTimeEntity {
+    @Column(nullable = false, updatable = false)
+    protected OffsetDateTime createdAt;
+    @Column(nullable = false)
+    protected OffsetDateTime updatedAt;
+    @PrePersist
+    void onCreate() {
+        createdAt = OffsetDateTime.now();
+        updatedAt = createdAt;
+    }
+    @PreUpdate
+    void onUpdate() {
+        updatedAt = OffsetDateTime.now();
+    }
+}
