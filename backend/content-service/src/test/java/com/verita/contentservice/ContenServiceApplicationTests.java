cd721b19c61aacecf486485cdfa2b3d15d2e93fc
Arpad-H <horvaarpad@gmail.com>
2026-06-11 13:34:05 +0200

    added @Testcontainers JUnit 5 extension

--- backend/content-service/src/test/java/com/verita/contentservice/ContenServiceApplicationTests.java
+++ backend/content-service/src/test/java/com/verita/contentservice/ContenServiceApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(TestcontainersConfiguration.class)
+@Testcontainers(disabledWithoutDocker = true)
 class ContenServiceApplicationTests {
 
     @Test
