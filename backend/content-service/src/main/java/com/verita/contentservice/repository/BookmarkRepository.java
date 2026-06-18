ecfee6f9319fd8061bddc0e7836699fcfdbe2a0c
Wenjie Zhu <wenjie.zhu@tum.de>
2026-06-18 20:10:15 +0200

    feat: add deleteByUserId in BookmarkRepository

--- backend/content-service/src/main/java/com/verita/contentservice/repository/BookmarkRepository.java
+++ backend/content-service/src/main/java/com/verita/contentservice/repository/BookmarkRepository.java
@@ -10,5 +10,6 @@ public interface BookmarkRepository extends JpaRepository<BookmarkEntity, UUID> 
     boolean existsByUserIdAndPost_Id(UUID userId, UUID postId);
     List<BookmarkEntity> findByUserId(UUID userId);
     List<BookmarkEntity> findByUserIdAndPost_IdIn(UUID userId, Collection<UUID> postIds);
+    void deleteByUserId(UUID userId);
     long countByPost_Id(UUID postId);
 }
